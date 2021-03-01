d39c8039ac3bb5a4251ab22e0bbc91b6416f34ec
Himanshu Thakur <himanshu.enest@gmail.com>
2021-03-01 15:23:04 +0530

    webhook misc

---
+++ WebHookDemo/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WebHookDemo.Controllers
+{
+	public class HomeController : ControllerBase
+	{
+		[GitHubWebHook]
+		public IActionResult GitHubHandler(JObject data)
+		{
+			var result = JsonConvert.SerializeObject(data);
+			return Ok(result);
+		}
+	}
+}
--- WebHookDemo/Startup.cs
+++ WebHookDemo/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,6 +17,11 @@ namespace WebHookDemo
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			//services.AddMvc(options =>
+			//{
+			//	options.EnableEndpointRouting = false;
+			//})
+			//	.AddGitHubWebHooks();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +33,8 @@ namespace WebHookDemo
 			}
 
 			app.UseRouting();
+
+			//app.UseMvc().UseMvcWithDefaultRoute();
 
 			app.UseEndpoints(endpoints =>
 			{
